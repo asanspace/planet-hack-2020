a55193b0d632e3ffce944b17167ee9c80e7f2d54
Moheeb Zara <virgilvox@Moheebs-MacBook-Pro.local>
2020-10-16 16:36:53 -0700

    okay

--- sonic/test.py
+++ sonic/test.py
@@ -5,6 +5,8 @@ import os
 import requests
 import time
 from sys import stdout
+import json 
+
 
 apikey = "edc98de4edd14e0c9cc393a6994876f4"
 
@@ -13,21 +15,17 @@ sdate = "2020-10-05"
 edate = "2020-10-07"
 limit = 16
 
+f = open('explorer-aoi.geojson',) 
+  
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+coords = data['features'][0]['geometry']['coordinates']
 aoi = {
   "type": "Polygon",
-  "coordinates": [
-    [
-      [-122.54, 37.81],
-      [-122.38, 37.84],
-      [-122.35, 37.71],
-      [-122.53, 37.70],
-      [-122.54, 37.81]
-    ]
-  ]
+  "coordinates": coords
 }
 
-# will pick up api_key via environment variable PL_API_KEY
-# but can be specified using `api_key` named argument
 client = api.ClientV1(api_key= "edc98de4edd14e0c9cc393a6994876f4")
 
 # build a query using the AOI and
@@ -42,33 +40,13 @@ request = filters.build_search_request(
     query, item_types=['PSScene3Band', 'PSScene4Band']
 )
 
-# if you don't have an API key configured, this will raise an exception
 results = client.quick_search(request)
 
 stdout.write('id,cloud_cover,date\n')
+coverVals = []
+for item in results.items_iter(limit=limit):
+    props = item['properties']
+    coverVals.append(item['properties']['cloud_cover'])
+    stdout.write('{0},{cloud_cover},{acquired}\n'.format(item['id'], **props))
 
-session = requests.Session()
-session.auth = ("edc98de4edd14e0c9cc393a6994876f4", '')
-# items_iter returns a limited iterator of all results. behind the scenes,
-# the client is paging responses from the API
-for item in results.items_iter(limit):
-        print(item['id'])
-        dataset = \
-            session.get(
-                ("https://api.planet.com/data/v1/item-types/" +
-                "{}/items/{}/assets/").format("PSScene4Band", item['id']))
-        # extract the activation url from the item for the desired asset
-        print(dataset.json())
-        item_activation_url = dataset.json()["udm"]["_links"]["activate"]
-        # request activation
-        response = session.post(item_activation_url)
-        print(response.status_code)
-        while response.status_code!=204:
-            time.sleep(30)
-            response = session.post(item_activation_url)
-            response.status_code = response.status_code
-            print(response.status_code)
-        assets = client.get_assets(item).get()
-        callback = api.write_to_file(directory=ddir, callback= None, overwrite= True)
-        body = client.download(assets["udm"], callback=callback)
-        body.wait()
+print(coverVals)
